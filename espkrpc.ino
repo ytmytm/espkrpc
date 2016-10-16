28ca11db1f2ba0e711c745db73ae78461df88cf7
ytmytm <mwitkowiak@gmail.com>
2016-10-16 16:06:20 +0200

    removed OTA stuff

--- espkrpc.ino
+++ espkrpc.ino
@@ -23,7 +23,6 @@ const char ssid[] = MYSSID;
 const char pass[] = MYPASSWORD;
 
 #include <ESP8266WiFi.h>
-#include <ArduinoOTA.h>
 
 // Use WiFiClient class to create TCP connections
 WiFiClient client;
@@ -182,41 +181,12 @@ void setup() {
   connect();
 
 
-  // OTA setup
-  // Port would default to 8266
-  ArduinoOTA.setPort(OTA_PORT);
-  // Hostname defaults to esp8266-[ChipID]
-  // ArduinoOTA.setHostname("myesp8266");
-  // No authentication by default
-  ArduinoOTA.setPassword((const char *)MYOTAPASSWORD);
-  ArduinoOTA.onStart([]() {
-    Serial.println("OTA Start");
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nOTA End");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("OTA Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("OTA Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
-  //
-
-
 }
 
 void loop() {
   yield();
   // reconnect if necessary
   connect();
-  ArduinoOTA.handle();
 
   /* Encode our message */
   {
