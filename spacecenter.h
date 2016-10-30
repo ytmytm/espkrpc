c91e95f2722978812e0ad4812f0d5ac456718333
ytmytm <mwitkowiak@gmail.com>
2016-10-30 11:24:16 +0100

    create argument list dynamically in each function

--- spacecenter.h
+++ spacecenter.h
@@ -9,18 +9,19 @@ class Flight {
   public:
     Flight(uint32_t flightid) {
       flightArgument = new KRPC::Argument(0, new KRPC::pbBytes(flightid));
-      args[0] = flightArgument;
     };
     ~Flight() {
       delete flightArgument;
     }
     double mean_altitude() {
+      KRPC::Argument *args[] = { flightArgument, NULL };
       KRPC::Request rq(SpaceCenterService, "Flight_get_MeanAltitude", args);
       double val;
       rq.getResponse(val);
       return val;
     };
     double surface_altitude() {
+      KRPC::Argument *args[] = { flightArgument, NULL };
       KRPC::Request rq(SpaceCenterService, "Flight_get_SurfaceAltitude", args);
       double val;
       rq.getResponse(val);
@@ -28,51 +29,48 @@ class Flight {
     };
   private:
     KRPC::Argument *flightArgument;
-    KRPC::Argument *args[2] = { NULL, NULL };
 };
 
 class Control {
   public:
     Control(uint32_t controlid) {
       controlArgument = new KRPC::Argument(0, new KRPC::pbBytes(controlid));
-      args[0] = controlArgument;
-      args2[0] = controlArgument;
     };
     ~Control() {
       delete controlArgument;
     }
     void activate_next_stage() {
+      KRPC::Argument *args[] = { controlArgument, NULL };
       KRPC::Request rq(SpaceCenterService, "Control_ActivateNextStage", args);
       rq.getResponse();
     };
     float throttle() {
+      KRPC::Argument *args[] = { controlArgument, NULL };
       KRPC::Request rq(SpaceCenterService, "Control_get_Throttle", args);
       float val;
       rq.getResponse(val);
       return val;
     };
-    void throttle(float thr) {
-      KRPC::Argument valueArgument = KRPC::Argument(1, new KRPC::pbBytes(thr));
-      args2[1] = &valueArgument;
-      KRPC::Request rq(SpaceCenterService, "Control_set_Throttle", args2);
+    void throttle(float value) {
+      KRPC::Argument valueArgument = KRPC::Argument(1, new KRPC::pbBytes(value));
+      KRPC::Argument *args[] = { controlArgument, &valueArgument, NULL };
+      KRPC::Request rq(SpaceCenterService, "Control_set_Throttle", args);
       rq.getResponse();
     };
   private:
     KRPC::Argument *controlArgument;
-    KRPC::Argument *args[2] = { NULL, NULL };
-    KRPC::Argument *args2[3] = { NULL, NULL, NULL };
 };
 
 class Orbit {
   public:
     Orbit(uint32_t orbitid) {
       orbitArgument = new KRPC::Argument(0, new KRPC::pbBytes(orbitid));
-      args[0] = orbitArgument;
     };
     ~Orbit() {
       delete orbitArgument;
     }
     double apoapsis_altitude() {
+      KRPC::Argument *args[] = { orbitArgument, NULL };
       KRPC::Request rq(SpaceCenterService, "Orbit_get_ApoapsisAltitude", args);
       double val;
       rq.getResponse(val);
@@ -80,21 +78,19 @@ class Orbit {
     };
   private:
     KRPC::Argument *orbitArgument;
-    KRPC::Argument *args[2] = { NULL, NULL };
 };
 
 class Resources {
   public:
     Resources(uint32_t resid) {
       resArgument = new KRPC::Argument(0, new KRPC::pbBytes(resid));
-      args[0] = resArgument;
     };
     ~Resources() {
       delete resArgument;
     }
     float amount(char *resource_name) {
       KRPC::Argument valueArgument = KRPC::Argument(1, new KRPC::pbBytes(resource_name));
-      args[1] = &valueArgument;
+      KRPC::Argument *args[] = { resArgument, &valueArgument, NULL };
       KRPC::Request rq(SpaceCenterService, "Resources_Amount", args);
       float val;
       rq.getResponse(val);
@@ -102,53 +98,47 @@ class Resources {
     };
   private:
     KRPC::Argument *resArgument;
-    KRPC::Argument *args[3] = { NULL, NULL, NULL };
 };
 
 class AutoPilot {
   public:
     AutoPilot(uint32_t autopilotid) {
       autopilotArgument = new KRPC::Argument(0, new KRPC::pbBytes(autopilotid));
-      args[0] = autopilotArgument;
-      args2[0] = autopilotArgument;
-      args3[0] = autopilotArgument;
     };
     ~AutoPilot() {
       delete autopilotArgument;
     }
     void engage() {
+      KRPC::Argument *args[] = { autopilotArgument, NULL };
       KRPC::Request rq(SpaceCenterService, "AutoPilot_Engage", args);
       rq.getResponse();
     };
     void disengage() {
+      KRPC::Argument *args[] = { autopilotArgument, NULL };
       KRPC::Request rq(SpaceCenterService, "AutoPilot_Disengage", args);
       rq.getResponse();
     };
     void set_target_pitch(float value) {
       KRPC::Argument valueArgument = KRPC::Argument(1, new KRPC::pbBytes(value));
-      args2[1] = &valueArgument;
-      KRPC::Request rq(SpaceCenterService, "AutoPilot_set_TargetPitch", args2);
+      KRPC::Argument *args[] = { autopilotArgument, &valueArgument, NULL };
+      KRPC::Request rq(SpaceCenterService, "AutoPilot_set_TargetPitch", args);
       rq.getResponse();
     };
     void set_target_heading(float value) {
       KRPC::Argument valueArgument = KRPC::Argument(1, new KRPC::pbBytes(value));
-      args2[1] = &valueArgument;
-      KRPC::Request rq(SpaceCenterService, "AutoPilot_set_TargetHeading", args2);
+      KRPC::Argument *args[] = { autopilotArgument, &valueArgument, NULL };
+      KRPC::Request rq(SpaceCenterService, "AutoPilot_set_TargetHeading", args);
       rq.getResponse();
     };
     void set_target_pitch_and_heading(float pitch, float heading) {
       KRPC::Argument valueArgument1 = KRPC::Argument(1, new KRPC::pbBytes(pitch));
       KRPC::Argument valueArgument2 = KRPC::Argument(2, new KRPC::pbBytes(heading));
-      args3[1] = &valueArgument1;
-      args3[2] = &valueArgument2;
-      KRPC::Request rq(SpaceCenterService, "AutoPilot_TargetPitchAndHeading", args3);
+      KRPC::Argument *args[] = { autopilotArgument, &valueArgument1, &valueArgument2, NULL };
+      KRPC::Request rq(SpaceCenterService, "AutoPilot_TargetPitchAndHeading", args);
       rq.getResponse();
     };
   private:
     KRPC::Argument *autopilotArgument;
-    KRPC::Argument *args[2] = { NULL, NULL };
-    KRPC::Argument *args2[3] = { NULL, NULL, NULL };
-    KRPC::Argument *args3[4] = { NULL, NULL, NULL, NULL };
 };
 
 class Vessel {
